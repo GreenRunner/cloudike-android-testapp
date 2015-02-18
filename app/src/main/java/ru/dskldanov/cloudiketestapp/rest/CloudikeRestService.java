5a2fa3d7c334995eef67543cd7a5cb38047c5809
green <muaitaibox@gmail.com>
2015-02-19 02:53:27 +0300

    add RoboSpice library dependency and created CloudikeRestService

---
+++ app/src/main/java/ru/dskldanov/cloudiketestapp/rest/CloudikeRestService.java
@@ -0,0 +1,13 @@
+package ru.dskldanov.cloudiketestapp.rest;
+
+
+import com.octo.android.robospice.Jackson2GoogleHttpClientSpiceService;
+
+/**
+ * Service for handling Cloudike Rest requests
+ * TODO: Add cache handling
+ * TODO: Think about thread count
+ */
+public class CloudikeRestService extends Jackson2GoogleHttpClientSpiceService {
+
+}
